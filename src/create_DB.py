a92a3a983cef82be0f522b0312a80044264d630b
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-09-05 23:43:36 +0500

    изменен процесс сохранения вакансии в БД, добавлена проверка на уникальность компании

--- src/create_DB.py
+++ src/create_DB.py
@@ -9,26 +9,26 @@ class CreateDB:
 
     def __init__(self, database_name):
         self.database_name = database_name
-        self.__db = self.__get_config()
+        self.__config_db = self.get_config()
 
 
-    def __get_config(self, filename:str=PATH_TO_DB_INI, section:str="postgresql") -> dict:
+    def get_config(self, filename:str=PATH_TO_DB_INI, section:str= "postgresql") -> dict:
         """ Метод для получения конфигурации базы данных """
         parser = ConfigParser()
         parser.read(filename)
-        self.__db = {}
+        self.__config_db = {}
         if parser.has_section(section):
             params = parser.items(section)
             for param in params:
-                self.__db[param[0]] = param[1]
-            return self.__db
+                self.__config_db[param[0]] = param[1]
+            return self.__config_db
         else:
             raise Exception(
                 'Section {0} is not found in the {1} file.'.format(section, filename))
 
     def create_new_db(self):
         """ Метод, который создает новую базу данных """
-        conn = psycopg2.connect(dbname='postgres', **self.__db)
+        conn = psycopg2.connect(dbname='postgres', **self.__config_db)
         conn.autocommit = True
         cur = conn.cursor()
 
@@ -37,7 +37,7 @@ class CreateDB:
 
         conn.close()
 
-        conn = psycopg2.connect(dbname=self.database_name, **self.__db)
+        conn = psycopg2.connect(dbname=self.database_name, **self.__config_db)
 
         with conn.cursor() as cur:
             cur.execute("""
--- src/save_vacancy_to_db.py
+++ src/save_vacancy_to_db.py
@@ -3,11 +3,15 @@ import psycopg2
 from src.create_DB import CreateDB
 
 
-class SaveVacancyInDB(CreateDB):
+class SaveVacancyToDB(CreateDB):
+    def __init__(self, database_name):
+        super().__init__(database_name)
+        self.__config_db = self.get_config()
+
     def save_vacancy_to_database(self, list_of_vacancies) -> None:
         """ Сохранение данных о вакансии в базу данных."""
 
-        conn = psycopg2.connect(dbname=self.database_name, **self.__db)
+        conn = psycopg2.connect(dbname=self.database_name, **self.__config_db)
 
         with conn.cursor() as cur:
             for vacancy in list_of_vacancies:
@@ -21,11 +25,12 @@ class SaveVacancyInDB(CreateDB):
                 experience = vacancy.experience
                 cur.execute(
                     """
-                    INSERT INTO companies (company_name)
-                    VALUES (%s)
-                    RETURNING company_id
+                        INSERT INTO companies (company_name)
+                        SELECT (%s)
+                        WHERE NOT EXISTS (SELECT 1 FROM companies WHERE company_name = (%s));
+                        SELECT company_id FROM companies WHERE company_name = (%s)
                     """,
-                    company_name
+                    (company_name, company_name, company_name)
                 )
                 company_id = cur.fetchone()[0]
                 cur.execute(
