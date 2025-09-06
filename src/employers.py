ce56e071a5d422f3e1b19c09577f65db35e0ae05
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-09-06 22:41:59 +0500

    добавлен класс Employers и методы сравнения по количеству открытых вакансий

---
+++ src/employers.py
@@ -0,0 +1,43 @@
+class Employers:
+    employer_id: int
+    name: str
+    open_vacancies: str
+
+    __slots__ = ('employer_id', 'name', 'open_vacancies')
+
+    def __init__(self, employer_id, name, open_vacancies):
+        self.employer_id = employer_id
+        self.name = name
+        self.open_vacancies = open_vacancies
+
+    @classmethod
+    def add_employer(cls, employer_dict: dict):
+        """Метод для создания объектов класса из словаря"""
+        return cls(**employer_dict)
+
+    @staticmethod
+    def __verify_data(other):
+        """Метод для проверки принадлежности объекта к классу int или Employers"""
+        if not isinstance(other, (int, Employers)):
+            raise TypeError("Сравнивать можно только с типом int или Employers")
+        return other if isinstance(other, int) else other.open_vacancies
+
+    def __eq__(self, other):
+        open_vacancies = self.__verify_data(other)
+        return self.open_vacancies == open_vacancies
+
+    def __gt__(self, other):
+        open_vacancies = self.__verify_data(other)
+        return self.open_vacancies > open_vacancies
+
+    def __ge__(self, other):
+        open_vacancies = self.__verify_data(other)
+        return self.open_vacancies >= open_vacancies
+
+    def __lt__(self, other):
+        open_vacancies = self.__verify_data(other)
+        return self.open_vacancies < open_vacancies
+
+    def __le__(self, other):
+        open_vacancies = self.__verify_data(other)
+        return self.open_vacancies <= open_vacancies
