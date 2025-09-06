16d0f82e3d1d29c92c4ba2810a460027afb78b73
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-09-06 22:43:28 +0500

    изменен метод get_vacancies теперь он возвращает словарь employer_id: list_vacancies

--- src/head_hunter_api.py
+++ src/head_hunter_api.py
@@ -1,10 +1,12 @@
 import requests
+
+from src.employers import Employers
 from src.vacancies import Vacancy
 from src.vacancy_parser import VacancyParser
 
 
 class HeadHunterAPI(VacancyParser):
-    """Класс для получения вакансий с сайта HH.ru"""
+    """Класс для получения вакансий и списка работодателей с сайта HH.ru"""
 
     def __init__(self):
         self.keyword = ""
@@ -31,60 +33,60 @@ class HeadHunterAPI(VacancyParser):
 
         return items
 
+
     def get_list_employers(self, employer_name):
-        employers = []
+        list_employers = []
         params = {"text": employer_name,
-                  'only_with_vacancies': True,
+                  "only_with_vacancies": True,
                   "page": self.__params["page"],
                   "per_page": self.__params["per_page"]}
-        list_employers = self.get_items('employers/', params)
+        employers = self.get_items('employers/', params)
 
-        for employer in list_employers:
-            employers.append({
-                'id': employer['id'],
-                'name': employer['name'],
-                'url': employer['alternate_url'],
-                'open_vacancies': employer['open_vacancies']
-            })
+        for employer in employers:
+            employer_dict = {
+                "id": employer["id"],
+                "name": employer["name"],
+                "url": employer["alternate_url"],
+                "open_vacancies": employer["open_vacancies"]
+            }
+            list_employers.append(Employers.add_employer(employer_dict))
 
-        return employers
+        return list_employers
 
-    def load_vacancies(self) -> list:
-        """Метод для получения списка вакансий с сервера"""
-        all_vacancies = []
-        self.__params["text"] = self.keyword
-        while self.__params["page"] < 20:
-            response = requests.get(
-                self.__base_url, headers=self.__headers, params=self.__params
-            )
 
-            if response.status_code == 200:
-                current_vacancies = response.json().get("items", [])
-                all_vacancies.extend(current_vacancies)
-                self.__params["page"] += 1
-            else:
-                raise ConnectionError(f"Ошибка запроса: {response.status_code}")
-        return all_vacancies
+    def get_vacancies(self, employer_ids:list, only_with_salary:bool, keyword: str = "") -> list[Vacancy]:
+        """ Метод для получения списка словарей вакансий по ключевому слову.
+            По умолчанию вакансии загружаются по слову 'python'. """
 
-    def get_vacancies(self, keyword: str = "python") -> list[Vacancy]:
-        """Метод для получения списка словарей вакансий по ключевому слову.
-        По умолчанию вакансии загружаются по слову 'python'."""
-        list_vacancies = []
         self.keyword = keyword
+        list_employers_and_vacancies = []
         try:
-            vacancies = self.load_vacancies()
-            for vacancy in vacancies:
-                vacancy_dict = {
-                    "job_title": vacancy.get("name"),
-                    "requirements": vacancy["snippet"]["requirement"],
-                    "responsibility": vacancy["snippet"]["responsibility"],
-                    "salary_from": vacancy["salary"]["from"] if vacancy["salary"] else "-",
-                    "salary_to": vacancy["salary"]["to"] if vacancy["salary"] else "-",
-                    "job_link": vacancy.get("alternate_url"),
-                    "employer_name": vacancy["employer"]["name"],
-                    "experience": vacancy["experience"]["name"]
-                }
-                list_vacancies.append(Vacancy.add_vacancy(vacancy_dict))
-            return list_vacancies
+            for employer_id in employer_ids:
+                params = {"employer_id": employer_id,
+                          "only_with_salary": only_with_salary,
+                          "currency": 'RUR',
+                          "page": self.__params["page"],
+                          "per_page": self.__params["per_page"],
+                          "text": self.keyword
+                        }
+
+                list_vacancies = []
+                vacancies = self.get_items('vacancies/', params)
+                for vacancy in vacancies:
+                    vacancy_dict = {
+                        "job_title": vacancy.get("name"),
+                        "requirements": vacancy["snippet"]["requirement"],
+                        "responsibility": vacancy["snippet"]["responsibility"],
+                        "salary_from": vacancy["salary"]["from"] if vacancy["salary"] else "-",
+                        "salary_to": vacancy["salary"]["to"] if vacancy["salary"] else "-",
+                        "job_link": vacancy.get("alternate_url"),
+                        "employer_name": vacancy["employer"]["name"],
+                        "experience": vacancy["experience"]["name"]
+                    }
+                    list_vacancies.append(Vacancy.add_vacancy(vacancy_dict))
+
+                list_employers_and_vacancies.append({employer_id: list_vacancies})
+            return list_employers_and_vacancies
+
         except ConnectionError:
-            return list_vacancies
+            return list_employers_and_vacancies
