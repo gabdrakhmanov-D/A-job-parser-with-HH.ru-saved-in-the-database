4c20cd32c62bf3c293573e3d5a109fbec5cb8f39
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-09-09 22:22:31 +0500

    реализована программа взаимодействия с пользователем и вспомогательные программы

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
-from config import PATH_TO_DB_INI
 from src.creator_db import CreateDB
+from src.db_manager import DBManager
 from src.head_hunter_api import HeadHunterAPI
 from src.save_vacancy_to_db import SaveVacancyToDB
+from src.utils import read_json, db_creation_request, get_list_titles_and_id, only_salary_request, \
+    get_command_from_user, get_result
 
 
-def user_interaction():
+def user_interaction() -> None:
     """Основная функция для запуска приложения"""
-    hh_api = HeadHunterAPI()
-    hh_vacancies = hh_api.get_vacancies([3529, 1740,9498112, 80, 991318, 2180, 445136, 1455, 2748, 78638], True, '')
 
-    new_db = CreateDB('vacancies')
-    new_db.create_new_db()
-    new_db.create_tables()
-    save = SaveVacancyToDB(new_db.database_name)
-    save.save_vacancy_to_database(hh_vacancies)
+    user_request_create_db = db_creation_request()
+    db_name = input('Введите название базы данных\n').lower()
+
+    if user_request_create_db == '1':
+        try:
+            new_db = CreateDB(db_name)
+            new_db.create_new_db()
+            new_db.create_tables()
+            print('База данных успешно создана')
+        except Exception as e:
+            return print(f'Ошибка создания базы данных: {e}')
+
+        try:
+            list_id_employers = read_json()
+        except FileNotFoundError as e:
+            return print(f'Файл с ID компаний не найден: {e}')
+        except Exception as e:
+            return print(f'Возникла ошибка при чтении файла: {e}')
+
+        ids_list, names_list = get_list_titles_and_id(list_id_employers)
+        only_with_salary = only_salary_request()
+
+        print('Сейчас будут загружены вакансии следующих компаний:')
+        for employer_name in names_list:
+            print(employer_name)
+
+        hh_api = HeadHunterAPI()
+        hh_vacancies = hh_api.get_vacancies(ids_list, only_with_salary)
+        save_vacancies = SaveVacancyToDB(new_db.database_name)
+        save_vacancies.save_vacancy_to_database(hh_vacancies)
+
+    db_manager = DBManager(db_name)
+    selector = get_command_from_user()
+    get_result(db_manager, selector)
+
 if __name__ == '__main__':
-    a =user_interaction()
-
-cv = [
-      {3529: 'СБЕР'},
-      {1740: 'Яндекс'},
-      {9498112: 'Яндекс Крауд'},
-      {80: 'Альфа-Банк'},
-      {991318: 'WINLINE'},
-      {2180: 'Ozon'},
-      {445136:'ООО АйТи-Солюшн'},
-      {1455: 'HeadHunter'},
-      {2748: 'Ростелеком'},
-      {78638: 'Т-Банк'}
-      ]
+    user_interaction()
